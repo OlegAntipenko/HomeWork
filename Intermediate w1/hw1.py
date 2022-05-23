8e07f35f3d95b3b002fa3492fcdf4cbb766435ce
OlegAntipenko <aopz@i.ua>
2022-05-23 23:46:01 +0300

    Работа над ошибками

--- Intermediate w1/hw1.py
+++ Intermediate w1/hw1.py
@@ -1,81 +1,111 @@
 class Mixin:
     @staticmethod
-    def __sub__(self, other):
-        return Fraction(self.__num - other.num, self.__den - other.den)
+    def sub(val1, val2):
+        if val1.den == val2.den:
+            new_num = val1.num - val2.num
+            new_den = val1.den
+        else:
+            new_num = val1.num * val2.den - val1.den * val2.num
+            new_den = val1.den * val2.den
+        return Fraction(new_num, new_den)
 
     @staticmethod
-    def __add__(self, other):
-        return Fraction(self.__num + other.num, self.__den + other.den)
+    def add(val1, val2):
+        if val1.den == val2.den:
+            new_num = val1.num + val2.num
+            new_den = val1.den
+        else:
+            new_num = val1.num * val2.den + val1.den * val2.num
+            new_den = val1.den * val2.den
+        return Fraction(new_num, new_den)
 
     @staticmethod
-    def __mul__(self, other):
-        return Fraction(self.__num * other.num, self.__den * other.den)
+    def mul(val1, val2):
+        return Fraction(val1.num * val2.num, val1.den * val2.den)
 
     @staticmethod
-    def __truediv__(self, other):
-        return Fraction(self.__num / other.num, self.__den / other.den)
+    def truediv(val1, val2):
+        return Fraction(val1.num * val2.den, val1.den * val2.num)
 
 
 class Fraction(Mixin):
     def __init__(self, num, den):
-        self.__num = num
-        self.__den = den
+        self.num = self.chek(num)
+        self.den = self.chek_den(den)
 
     @staticmethod
     def chek(x):
         if type(x) != int:
-            raise TypeError("Числа должны быть целым")
+            raise Exception("Числа должны быть целым")
+        return x
+
+    @staticmethod
+    def chek_den(y):
+        if isinstance(y, int) and y != 0:
+            return y
+        else:
+            raise Exception("Числа должны быть целым и больше нуля")
 
     @property
     def num(self):
-        return self.__num
+        return self.num
 
     @num.setter
     def num(self, num):
-        self.chek(num)
-        self.__num = num
+        self.num = self.chek(num)
 
     @num.deleter
     def num(self):
-        del self.__num
+        del self.num
 
     @property
     def den(self):
-        return self.__den
+        return self.den
 
     @den.setter
     def den(self, den):
-        self.chek(den)
-        self.__den = den
+        self.den = self.chek_den(den)
 
     @den.deleter
     def den(self):
-        del self.__den
+        del self.den
 
     def __sub__(self, other):
-        return Fraction(self.__num - other.num, self.__den - other.den)
+        if self.den == other.den:
+            new_num = self.num - other.num
+            new_den = self.den
+        else:
+            new_num = self.num * other.den - self.den * other.num
+            new_den = self.den * other.den
+        return Fraction(new_num, new_den)
 
     def __add__(self, other):
-        return Fraction(self.__num + other.num, self.__den + other.den)
+        if self.den == other.den:
+            new_num = self.num + other.num
+            new_den = self.den
+        else:
+            new_num = self.num * other.den + self.den * other.num
+            new_den = self.den * other.den
+        return Fraction(new_num, new_den)
 
     def __mul__(self, other):
-        return Fraction(self.__num * other.num, self.__den * other.den)
+        return Fraction(self.num * other.num, self.den * other.den)
 
     def __truediv__(self, other):
-        return Fraction(self.__num / other.num, self.__den / other.den)
+        return Fraction(self.num * other.den, self.den * other.num)
 
     @classmethod
     def string(cls, st):
-        st = cls.st.split('/')
-        num = cls.st[0]
-        den = cls.st[1]
-        return Fraction(num, den)
+        st = st.split('/')
+        num = st[0]
+        den = st[1]
+        return cls(num, den)
 
     def __str__(self):
         return f"{self.num}/{self.den}"
 
 
-f1 = Fraction(8, 10)
+f1 = Fraction(2, 3)
 f1.num = 10
 f2 = Fraction(5, 4)
 f2.den = 8
@@ -83,3 +113,7 @@ print(f1 - f2)
 print(f1 + f2)
 print(f1 * f2)
 print(f1 / f2)
+print(Fraction.add(f1, f2))
+print(Fraction.sub(f1, f2))
+print(Fraction.mul(f1, f2))
+print(Fraction.truediv(f1, f2))
